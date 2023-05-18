7713c673406f21b1ac41317091703f101e82d79b
d3trim3ntal <obsidianblackrose@gmail.com>
2023-05-18 12:16:37 -0700

    Fixed colliders and Title scene in progress
    progress made

--- PsychZentenoP3Final/Assets/Scripts/PlayerControllerr.cs
+++ PsychZentenoP3Final/Assets/Scripts/PlayerControllerr.cs
@@ -12,7 +12,8 @@ public class PlayerControllerr : MonoBehaviour
     public float gravity = 10f;
 
     public float lookSpeed = 2f;
-    public float lookXLimit = 45f;
+    public float lookXLimit = 50f;
+    public float lookXLimitTwo = 70f;
 
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
@@ -50,7 +51,7 @@ public class PlayerControllerr : MonoBehaviour
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimitTwo);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
